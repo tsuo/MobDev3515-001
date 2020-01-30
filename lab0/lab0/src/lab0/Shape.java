0e9502eeb97c9007a04009fc0c5b1313181cf56a
Dan Tran <tug06533@temple.edu>
2020-01-30 09:30:34 -0500

    finished Shape class

--- lab0/lab0/src/lab0/Shape.java
+++ lab0/lab0/src/lab0/Shape.java
@@ -10,5 +10,24 @@ package lab0;
  * @author tsuoi
  */
 public class Shape {
+    private String name;
     
+    public Shape(String name){
+        this.name = name;
+    }
+    
+    /** returns the name of the shape **/
+    public String getName(){
+        return name;
+    }
+    
+    /** return the area of the shape **/
+    public double getArea(){
+        return 0.0;
+    }
+    
+    public void printDimensions()
+    {
+        System.out.println("No dimensions");
+    }
 }
