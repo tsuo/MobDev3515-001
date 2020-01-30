e6a3981f469c5ea99db1668deeee953d04889cf5
Dan Tran <tug06533@temple.edu>
2020-01-30 10:18:26 -0500

    added event listener to button to change ttext

--- lab1/app/src/main/java/edu/temple/helloworld/MainActivity.java
+++ lab1/app/src/main/java/edu/temple/helloworld/MainActivity.java
@@ -3,6 +3,8 @@ package edu.temple.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.id_btn_change_text).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((TextView)findViewById(R.id.id_txt_title_text)).setText(R.string.changed_text);
+            }
+        });
     }
 }
